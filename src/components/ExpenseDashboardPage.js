a4fa0d5a17e2d86f513fede0971d18d484e87945
Norman Ozier <normdefcon@hotmail.com>
2018-03-22 10:56:37 +0800

    Add ExpensesSummary to Dashboard

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilters';
-
+import ExpensesSummary from './ExpensesSummary'
 
 
 //Expense list needs nothing passed down and is rendered just like that
 //Provided by connect
 const ExpenseDashboardPage = () => (
     <div>
+        <ExpensesSummary />
         <ExpenseListFilters/>
         <ExpenseList />
     </div>
